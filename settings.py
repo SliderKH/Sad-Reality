2ca391ea4d4b5024e32badc84cad47e21d6ff29c
Slider <slider.kh@gmail.com>
2022-10-09 23:14:10 +0200

    add settings

---
+++ settings.py
@@ -0,0 +1,4 @@
+token = ''
+logo_url = 'https://cdn.discordapp.com/attachments/897514082763161630/1028697377114161234/SR.jpg'
+member_role_id = 897515410994372639
+welcome_chat_id = 897515333664002058
